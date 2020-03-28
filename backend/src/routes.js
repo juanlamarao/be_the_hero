1de9e36c9e38de96c70437b8966144e9bdcaefd5
Juan Carlos Lamarão <oliveiralamarao@gmail.com>
2020-03-28 19:48:58 -0300

    backend finalizado

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const ongController = require('./Controllers/ongController');
+const incidentController = require('./Controllers/incidentController');
+const profileController = require('./Controllers/profileController');
+const sessionController = require('./Controllers/sessionController');
+
+const routes = express.Router();
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+
+routes.post('/sessions', sessionController.create)
+routes.get('/profile', profileController.index);
+
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
+
+module.exports = routes;
